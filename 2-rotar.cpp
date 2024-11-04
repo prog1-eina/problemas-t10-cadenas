c0a533d044c4589f7885596f38482fc6b48b359d
Miguel Ángel Latre Abadía <latre@unizar.es>
2024-11-04 18:05:24 +0100

    Update line length to 120 chars

--- 2-rotar.cpp
+++ 2-rotar.cpp
@@ -1,19 +1,17 @@
-﻿/******************************************************************************\
+﻿/**********************************************************************************************************************\
  * Curso de Programación 1. Tema 10 (Caracteres y cadenas de caracteres)
  * Autores: Miguel Ángel Latre
  * Última revisión: 31 de octubre de 2024
- * Resumen: Soluciones a los problemas de Programación 1 planteados en la clase
- *          de problemas del tema 10.
- * Nota: El programa completo está contenido en este fichero, por lo que puede 
- *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
- *       Code.
+ * Resumen: Soluciones a los problemas de Programación 1 planteados en la clase de problemas del tema 10.
+ * Nota: El programa completo está contenido en este fichero, por lo que puede  compilarse y ejecutarse con la extensión
+ *       Code Runner de Visual Studio Code.
  *       También puede compilarse desde la terminal través de la orden
  *           g++ -Wall -Wextra 2-rotar.cpp -o 2-rotar
  *       y ejecutarse en Windows a través de la orden
  *           .\2-rotar.exe
  *       o en Linux y macOS
  *           ./2-rotar
-\******************************************************************************/
+\**********************************************************************************************************************/
 
 #include <iostream>
 #include <string>
@@ -22,10 +20,8 @@ using namespace std;
 
 /*
  * Pre:  ---
- * Post: Modifica «cadena» de forma que mueve del principio al final de esta su
- *       primer carácter, manteniendo el resto de los caracteres en el mismo
- *       orden en el que estaban inicialmente, que quedan al principio de
- *       «cadena».
+ * Post: Modifica «cadena» de forma que mueve del principio al final de esta su primer carácter, manteniendo el resto de
+ *       los caracteres en el mismo orden en el que estaban inicialmente, que quedan al principio de «cadena».
  */
 void rotar(string &cadena) {
     if (cadena.length() > 0) {
@@ -40,12 +36,10 @@ void rotar(string &cadena) {
 
 /*
  * Pre:  ---
- * Post: Modifica «cadena» de forma que mueve del principio al final de esta su
- *       primer carácter, manteniendo el resto de los caracteres en el mismo
- *       orden en el que estaban inicialmente, que quedan al principio de
- *       «cadena».
- * Nota: Esta versión utiliza métodos de la clase «string» que no se vieron en
- *       clase, pero que se pueden utilizar. En concreto, utiliza los métodos
+ * Post: Modifica «cadena» de forma que mueve del principio al final de esta su primer carácter, manteniendo el resto de
+ *       los caracteres en el mismo orden en el que estaban inicialmente, que quedan al principio de «cadena».
+ * Nota: Esta versión utiliza métodos de la clase «string» que no se vieron en clase, pero que se pueden utilizar. En
+ *       concreto, utiliza los métodos
  *       «empty()» (https://cplusplus.com/reference/string/string/empty/) y
  *       «substr()» (https://cplusplus.com/reference/string/string/substr/).
  */
@@ -56,7 +50,7 @@ void rotar2(string &cadena) {
 }
 
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 /*
  * Programa de pruebas del procedimiento «rotar».
  */
